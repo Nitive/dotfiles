5127adbe0133237f07743c0c7eeead72284bd265
Maxim Samoilov <nitive@icloud.com>
2021-08-03 11:47:05 +0300

    Disable tracking in CLI utilites

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -48,3 +48,9 @@ end
 
 # Bind m in command vi mode to opening current command in editor
 bind --mode default m edit_command_buffer
+
+export DO_NOT_TRACK=1
+export GATSBY_TELEMETRY_DISABLED=1
+export HOMEBREW_NO_ANALYTICS=1
+export STNOUPGRADE=1
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
